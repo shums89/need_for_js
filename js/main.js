b403caadcad1f2640ef951c01c239274989cff6d
Александр <sh889@ya.ru>
2021-06-25 15:07:00 +0500

    Доработки

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
+"use strict";
+
+const MAX_ENEMY = 7;
+const HEIGT_ELEM = 100;
+
 const score = document.querySelector('.score'),
   start = document.querySelector('.start'),
   gameArea = document.querySelector('.gameArea'),
-  car = document.createElement('div');
+  car = document.createElement('div'),
+  btns = document.querySelectorAll('.btn');
+
+
+let startSpeed = 0;
+/*
+// вариант 1 для аудио
+  const music = document.createElement('embed');
+  music.src = './audio/music.mp3';
+  music.classList.add('visually-hidden');
+*/
+
+// вариант 2 для аудио
+const music = new Audio('./audio/music.mp3');
 
 car.classList.add('car');
+gameArea.classList.add('hide');
+score.classList.add('hide');
 
 const keys = {
   ArrowUp: false,
@@ -15,44 +35,87 @@ const keys = {
 const setting = {
   start: false,
   score: 0,
-  speed: 3,
+  speed: 5,
   traffic: 3
 };
 
 function getQuantityElements(heightElement) {
-  return document.documentElement.clientHeight / heightElement + 1;
+  return (gameArea.offsetHeight / heightElement) + 1;
 }
 
-start.addEventListener('click', startGame);
-document.addEventListener('keydown', startRun);
-document.addEventListener('keyup', stopRun);
+function getRandomEnemy(max) {
+  return Math.floor((Math.random() * max) + 1);
+}
 
-function startGame() {
-  start.classList.add('hide');
-  gameArea.innerHTML = '';
+function changeLevel(lvl) {
+  switch (lvl) {
+    case '1':
+      setting.traffic = 4;
+      setting.speed = 3;
+      break;
+    case '2':
+      setting.traffic = 3;
+      setting.speed = 6;
+      break;
+    case '3':
+      setting.traffic = 3;
+      setting.speed = 8;
+      break;
+  }
+  startSpeed = setting.speed;
+}
 
-  for (let i = 0; i < getQuantityElements(100); i++) {
-    const line = document.createElement('div');
-    line.classList.add('line');
-    line.style.top = `${(i * 100)}px`;
-    line.y = i * 100;
-    gameArea.appendChild(line);
+function startGame(event) {
+  //document.body.append(music);
+
+  const target = event.target;
+
+  if (!target.classList.contains('btn')) {
+    return;
   }
 
-  for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
+  const levelGame = target.dataset.levelGame;
+  changeLevel(levelGame);
+
+  btns.forEach(btn => {
+    btn.disabled = true;
+  });
+
+  music.volume = 0.05;
+  music.play();
+  //start.classList.add('hide');
+  gameArea.classList.remove('hide');
+  score.classList.remove('hide');
+  gameArea.innerHTML = '';
+  gameArea.style.minHeight = `${Math.floor((document.documentElement.clientHeight - HEIGT_ELEM) / HEIGT_ELEM) * HEIGT_ELEM}px`;
+
+  for (let i = 0; i < getQuantityElements(HEIGT_ELEM); i++) {
+    const line = document.createElement('div');
+    line.classList.add('line');
+    line.style.top = `${i * HEIGT_ELEM}px`;
+    line.style.height = `${HEIGT_ELEM / 2}px`;
+    line.y = i * HEIGT_ELEM;
+    gameArea.append(line);
+  }
+
+  for (let i = 0; i < getQuantityElements(HEIGT_ELEM * setting.traffic); i++) {
     const enemy = document.createElement('div');
     enemy.classList.add('enemy');
-    enemy.y = -100 * setting.traffic * (i + 1);
+    enemy.y = -HEIGT_ELEM * setting.traffic * (i + 1);
     enemy.style.left = `${Math.floor(Math.random() * (gameArea.offsetWidth - 50))}px`;
     enemy.style.top = `${enemy.y}px`;
-    enemy.style.background = 'transparent url(../img/enemy.png) center / cover no-repeat';
-    gameArea.appendChild(enemy);
+    enemy.style.background = `
+        transparent 
+        url(../img/enemy${getRandomEnemy(MAX_ENEMY)}.png) 
+        center / cover 
+        no-repeat`;
+    gameArea.append(enemy);
   }
 
   setting.score = 0;
   setting.start = true;
-  gameArea.appendChild(car);
-  car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2;
+  gameArea.append(car);
+  car.style.left = `${gameArea.offsetWidth / 2 - car.offsetWidth / 2}px`;
   car.style.top = 'auto';
   car.style.bottom = '10px';
   setting.x = car.offsetLeft;
@@ -63,7 +126,10 @@ function startGame() {
 function playGame() {
   if (setting.start) {
     setting.score += setting.speed;
-    score.innerHTML = `SCORE<br>${setting.score}`;
+    score.innerHTML = `SCORE: ${setting.score}`;
+    //start.style.top = `${score.offsetHeight}px`;
+
+    setting.speed = startSpeed + Math.floor(setting.score / 5000);
 
     moveRoad();
     moveEnemy();
@@ -92,13 +158,17 @@ function playGame() {
 }
 
 function startRun(event) {
-  event.preventDefault();
-  keys[event.key] = true;
+  if (keys.hasOwnProperty(event.key)) {
+    event.preventDefault();
+    keys[event.key] = true;
+  }
 }
 
 function stopRun(event) {
-  event.preventDefault();
-  keys[event.key] = false;
+  if (keys.hasOwnProperty(event.key)) {
+    event.preventDefault();
+    keys[event.key] = false;
+  }
 }
 
 function moveRoad() {
@@ -108,8 +178,8 @@ function moveRoad() {
     item.y += setting.speed;
     item.style.top = `${item.y}px`;
 
-    if (item.y >= document.documentElement.clientHeight) {
-      item.y = -100;
+    if (item.y >= gameArea.offsetHeight) {
+      item.y = -HEIGT_ELEM;
     }
   });
 }
@@ -123,19 +193,26 @@ function moveEnemy() {
 
     if (carRect.top <= enemyRect.bottom && carRect.right >= enemyRect.left && carRect.left <= enemyRect.right && carRect.bottom >= enemyRect.top) {
       setting.start = false;
+      //music.remove();
+      music.pause();
       console.warn('ДТП');
-      start.classList.remove('hide');
-      start.style.top = score.offsetHeight;
+      //start.classList.remove('hide');
+      //start.style.top = `${score.offsetHeight}px`;
+      btns.forEach(btn => {
+        btn.disabled = false;
+      });
     }
 
     item.y += setting.speed / 2;
     item.style.top = `${item.y}px`;
 
-    if (item.y >= document.documentElement.clientHeight) {
-      item.y = -100 * setting.traffic;
+    if (item.y >= gameArea.offsetHeight) {
+      item.y = -HEIGT_ELEM * setting.traffic;
       item.style.left = `${Math.floor(Math.random() * (gameArea.offsetWidth - 50))}px`;
     }
   });
-
-
 }
+
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
